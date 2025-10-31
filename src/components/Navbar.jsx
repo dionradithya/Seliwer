3ec510b3db9387732e5e34ae226f563fcc3639a0
dionradithya <dradithya37@gmail.com>
2025-10-31 22:20:21 +0700

    add navbar (unfinished)

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <nav
+      style={{
+        height: "85px",
+        backgroundColor: "#000",
+        color: "white",
+        display: "flex",
+        alignItems: "center",
+        paddingLeft: "20px",
+      }}
+    >
+      Navbar
+    </nav>
+  );
+}
+
+export default Navbar;
