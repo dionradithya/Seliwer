248b261892ce7e81c7dd4defdecaefd686c1cb20
dionradithya <dradithya37@gmail.com>
2025-11-12 03:23:00 +0700

    adding Card Section (Finished)

---
+++ src/pages/Home/CardSection.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Card from '../../components/Card'
+import Img1 from '../../assets/img/Card1.svg'
+import Img2 from '../../assets/img/Card2.svg'
+import Img3 from '../../assets/img/Card3.svg'
+import Img4 from '../../assets/img/Card4.svg'
+
+const CardSection = () => {
+  return (
+    <div style={{backgroundColor:'aliceblue', height: 'calc(100vh - 70px)', padding:'100px', textAlign:'center'}}>
+
+        {/* Card Section Text */}
+        <div style={{fontSize:'30px', fontWeight:'bold', marginBottom:'50px'}}>Kenapa Harus Seliwer</div>
+
+        {/* Card Groups */}
+        <div style={{display: "flex", justifyContent: "center", gap: "55px", flexWrap: "wrap"}}>
+            <Card image={Img1} title="Satu Tempat untuk Semua UMKM"/>
+            <Card image={Img2} title="Pencarian Cepat & Akurat"/>
+            <Card image={Img3} title="Informasi Lengkap & Terpercaya"/>
+            <Card image={Img4} title="Dukung UMKM Lokal Berkembang"/>
+        </div>
+
+    </div>
+  )
+}
+
+export default CardSection
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import HeroSection from './HeroSection'
 import Footer from "../../components/Footer/Footer"
 import UMKMSection from './UMKMSection';
+import CardSection from './CardSection';
 
 const Home = () => {
   return (
     <div>
       <HeroSection />
       <UMKMSection />
+      <CardSection />
       <Footer />
     </div>
   );
