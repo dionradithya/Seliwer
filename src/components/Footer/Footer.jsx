d448ffbc5ddeb14c80e8d244088e3fd003d0d38e
dionradithya <dradithya37@gmail.com>
2025-11-12 02:33:09 +0700

    adding hero section

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer style={{backgroundColor: "#D03068", color: "#fff", padding: "60px 246px", textAlign: "center" }}>
+    <footer style={{backgroundColor: "#0B632C", color: "#fff", padding: "60px 246px", textAlign: "center" }}>
 
         {/* MAIN GROUP */}
         <div style={{display:"flex", justifyContent: "space-between"}}>
@@ -11,7 +11,7 @@ const Footer = () => {
             <div style={{display: "flex", flexDirection: "column", textAlign:"start", width:"40%" }}>
                 <div style={{fontSize: "24px", backgroundColor:"#dd799cff"}}>LOGO</div>
                 <div style={{fontSize: "24px", marginTop: "15px", fontWeight: "bold"}}>Tempatnya kamu kenal, jelajahi, dan dukung UMKM sekitar.</div>
-                <div style={{fontSize: "16px", marginTop: "15px"}}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores consequatur</div>
+                <div style={{fontSize: "20px", marginTop: "15px"}}>Tempatnya kamu kenal, jelajahi, dan dukung UMKM sekitar.</div>
             </div>
 
             {/* KOLOM 2 */}
@@ -31,7 +31,7 @@ const Footer = () => {
                 <div style={{fontSize:"15px", padding:"8px 0px"}}>Youtube</div>
             </div>
         </div>
-        <div style={{marginTop: "20px"}}>© 2025 Dion. All rights reserved.</div>
+        <div style={{marginTop: "50px"}}>Copyright © 2025 Seliwer Indonesia</div>
     </footer>
   );
 };
